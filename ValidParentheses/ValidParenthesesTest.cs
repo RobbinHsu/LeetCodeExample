de945d38220eb7287805c193234c7120d7a66de9
祐瑋 許 <robbinhsu@outlook.com>
2020-07-31 23:25:05 +0800

    refactor test

--- ValidParentheses/ValidParenthesesTest.cs
+++ ValidParentheses/ValidParenthesesTest.cs
@@ -7,29 +7,48 @@ namespace ValidParentheses
 {
     public class ValidParenthesesTests
     {
+        private bool _actual;
+        private string _input;
+        private ValidParentheses _validParentheses;
+
+        public ValidParenthesesTests()
+        {
+            _validParentheses = new ValidParentheses();
+        }
+
         [Test]
         public void Test1()
         {
-            var input = "()";
+            GivenInput("()");
 
-            var validParentheses = new ValidParentheses();
-            var actual = validParentheses.GetOutput(input);
-            var expected = true;
-            Assert.AreEqual(expected, actual);
-            Assert.Pass();
+            WhenValidParentheses();
+
+            ShouldBe(true);
         }
-
 
         [Test]
         public void Test2()
         {
-            var input = "";
+            GivenInput("");
 
-            var validParentheses = new ValidParentheses();
-            var actual = validParentheses.GetOutput(input);
-            var expected = true;
-            Assert.AreEqual(expected, actual);
-            Assert.Pass();
+            WhenValidParentheses();
+
+            ShouldBe(true);
+        }
+
+        private void ShouldBe(bool compareResult)
+        {
+            Assert.AreEqual(compareResult, _actual);
+        }
+
+        private void WhenValidParentheses()
+        {
+            _actual = _validParentheses.GetOutput(_input);
+        }
+
+        private void GivenInput(string input)
+        {
+            _input = input;
         }
     }
 }
