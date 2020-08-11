bf0a7196e3f7cf54e4fadc6e45c10fa294a99ee4
祐瑋 許 <robbinhsu@outlook.com>
2020-08-11 21:59:20 +0800

    Test1 OK

--- LongestSubstringWithoutRepeatingCharacters/Solution.cs
+++ LongestSubstringWithoutRepeatingCharacters/Solution.cs
@@ -1,10 +1,24 @@
+using System.Collections.Generic;
+using System.Linq;
+
 namespace LongestSubstringWithoutRepeatingCharacters
 {
     public class Solution
     {
         public int LengthOfLongestSubstring(string s)
         {
-            return 0;
+            var characters = new List<char>();
+            
+            foreach (var character in s)
+            {
+                var find = characters.Find(x => x == character);
+                if (find != character)
+                {
+                    characters.Add(character);
+                }
+            }
+
+            return characters.Count;
         }
     }
 }
